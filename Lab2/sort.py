ea4a0caf5b923156ded598f1c99783c68bd5d9e4
ashutoshbr <ashutoshbr@proton.me>
2023-04-16 19:38:20 +0545

    Implement and test merge sort

--- Lab2/sort.py
+++ Lab2/sort.py
@@ -9,5 +9,38 @@ def insertion_sort(array: list):
     return array
 
 
-def merge_sort(array: list):
-    pass
+def merge(array: list, start: int, mid: int, end: int):
+    left_array = array[start:mid]
+    right_array = array[mid:end]
+
+    left_index = 0
+    right_index = 0
+    array_index = start
+
+    while left_index < len(left_array) and right_index < len(right_array):
+        if left_array[left_index] <= right_array[right_index]:
+            array[array_index] = left_array[left_index]
+            left_index = left_index + 1
+        else:
+            array[array_index] = right_array[right_index]
+            right_index = right_index + 1
+        array_index = array_index + 1
+
+    while left_index < len(left_array):
+        array[array_index] = left_array[left_index]
+        left_index = left_index + 1
+        array_index = array_index + 1
+    while right_index < len(right_array):
+        array[array_index] = right_array[right_index]
+        right_index = right_index + 1
+        array_index = array_index + 1
+
+
+def merge_sort(array: list, start: int, end: int):
+    if start >= end:
+        return array
+    mid = (start + end) // 2
+    merge_sort(array, start, mid)
+    merge_sort(array, mid + 1, end)
+    merge(array, start, mid, end)
+    return array
--- Lab2/test_sort.py
+++ Lab2/test_sort.py
@@ -1,6 +1,6 @@
 import unittest
 
-from sort import insertion_sort
+from sort import insertion_sort, merge_sort
 
 
 array = [56, 23, 68, 12, 99, 2, 15]
@@ -10,6 +10,13 @@ sorted_data = [2, 12, 15, 23, 56, 68, 99]
 class TestInsertionSort(unittest.TestCase):
     def test_sorted(self):
         obtained = insertion_sort(array)
+        expected = sorted_data
+        self.assertEqual(obtained, expected)
+
+
+class TestMergeSort(unittest.TestCase):
+    def test_sorted(self):
+        obtained = merge_sort(array, 0, len(array) - 1)
         expected = sorted_data
         self.assertEqual(obtained, expected)
 
